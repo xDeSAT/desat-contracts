92a889619883d538b5c54fc8e29e5d0ad8de5e6a
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-05-10 17:32:49 +0300

    docs: improve documentation

--- contracts/interfaces/IERC7578.sol
+++ contracts/interfaces/IERC7578.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.21;
 
 /**
- * @notice A struct containing data for redemption properties
- * @param tokenIssuer The network or entity minting the tokens
+ * @notice Struct encapsulating fields required to by the ERC-7578 standard to represent the physical asset
+ * @param tokenIssuer The network or entity minting the token
  * @param assetHolder The legal owner of the physical asset
  * @param storedLocation The physical storage location
  * @param terms Link to IPFS contract, agreement or terms
@@ -20,7 +20,7 @@ struct Properties {
 }
 
 /**
- * @notice A struct for amount values
+ * @notice Struct encapsulating fields describing the declared value of the physical asset
  * @param currency The currency of the amount
  * @param value The value of the amount
  */
@@ -34,28 +34,34 @@ struct Amount {
  */
 interface IERC7578 {
     /**
-     * @notice Emitted when properties are set
+     * @notice Emitted when the properties of the `tokenId` token are set
+     *
      * @param tokenId The ID of the token
      * @param properties The properties of the token
      */
     event PropertiesSet(uint256 indexed tokenId, Properties properties);
 
     /**
-     * @notice Emitted when properties are removed
+     * @notice Emitted when the properties of the `tokenId` token are removed
+     *
      * @param tokenId The ID of the token
      * @param properties The properties of the token
      */
     event PropertiesRemoved(uint256 indexed tokenId, Properties properties);
 
     /**
-     * @notice Retrieves all the properties of a token
+     * @notice Retrieves all the properties of the `tokenId` token
+     * @dev Does NOT revert if token doesn't exist
      * @param tokenId The token ID of the minted token
      */
     function getProperties(uint256 tokenId) external view returns (Properties memory properties);
 
     /**
-     * @notice Properties required to be set when minting a token
-     * @param tokenId The token ID of the minted token
+     * @notice Sets the properties of the `tokenId` token
+     *
+     * IMPORTANT: Properties required to be set when minting a token
+     *
+     * @param tokenId The ID of the token
      * @param properties The properties of the token
      */
     function setProperties(uint256 tokenId, Properties calldata properties) external;
